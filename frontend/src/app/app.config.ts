0c494ee467f022f517d8e0946581e5ebb7e8bc4a
Andrew <drewstandes@gmail.com>
2024-11-05 22:38:08 +0000

    ngx markdown

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,11 +7,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MARKED_OPTIONS, provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideMarkdown(),
   ],
 };
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,22 +3,21 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StreamService } from '../services/stream.service';
 import { ConfigService } from '../services/config.service';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { marked } from 'marked';
 import { Subscription } from 'rxjs';
 import { ClickOutsideDirective } from '../directives/click-outside.directive';
+import { MarkdownModule } from 'ngx-markdown';
+import { ExampleQuestion } from '../models/example-question';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, ClickOutsideDirective],
+  imports: [CommonModule, FormsModule, ClickOutsideDirective, MarkdownModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
 export class HomeComponent {
   userQuestion = '';
   answer = signal(''); // Raw markdown
-  renderedAnswer = signal<SafeHtml>(''); // Rendered HTML
   isLoading = signal(false);
   error = signal(false); // Tracks if an error occurred
   errorMessage = signal(''); // Stores the error message
@@ -27,10 +26,37 @@ export class HomeComponent {
   showTechStackTooltip = false;
   eventSourceSubscription: Subscription | null = null;
 
+  // Dynamic example questions
+  exampleQuestions: ExampleQuestion[] = [
+    {
+      emoji: '🥗',
+      text: 'What is intermittent fasting?',
+    },
+    {
+      emoji: '🦠',
+      text: 'What is a gut microbiome?',
+    },
+    {
+      emoji: '🌱',
+      text: 'What are the benefits of a plant-based diet?',
+    },
+    {
+      emoji: '🏃',
+      text: 'How does exercise affect mental health?',
+    },
+    {
+      emoji: '😴',
+      text: 'What role does sleep play in overall health?',
+    },
+    {
+      emoji: '🧠',
+      text: 'How does nutrition affect brain function?',
+    },
+  ];
+
   constructor(
     private configService: ConfigService,
-    private streamService: StreamService,
-    private sanitizer: DomSanitizer
+    private streamService: StreamService
   ) {}
 
   submitQuestion() {
@@ -44,17 +70,9 @@ export class HomeComponent {
     this.eventSourceSubscription = this.streamService
       .connectToServerSentEvents(url)
       .subscribe({
-        next: async (data: string) => {
+        next: (data: string) => {
           this.error.set(false); // Reset error state on successful data
-
-          this.answer.update((current) => current + data);
-
-          // Parse the markdown and update the rendered HTML
-          const updatedMarkdown = this.answer();
-          const html = await marked(updatedMarkdown);
-          this.renderedAnswer.update(() =>
-            this.sanitizer.bypassSecurityTrustHtml(html)
-          );
+          this.answer.update((current) => current + data); // Append incoming markdown data
         },
         complete: () => {
           this.isLoading.set(false);
@@ -73,7 +91,6 @@ export class HomeComponent {
       this.eventSourceSubscription.unsubscribe();
     }
     this.answer.set('');
-    this.renderedAnswer.set(''); // Reset the rendered answer
     this.isLoading.set(true);
     this.error.set(false); // Reset error state
     this.errorMessage.set(''); // Clear any previous error messages
@@ -96,7 +113,7 @@ export class HomeComponent {
   }
 
   closePopup() {
-    this.showPopup = false; // Close the popup when user acknowledges the warning
+    this.showPopup = false;
   }
 
   toggleTechStackTooltip() {
