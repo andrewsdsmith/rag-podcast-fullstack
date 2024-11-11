90a4cd52caaca360daa047b2e88bf2ccd2e3bf91
Andrew <drewstandes@gmail.com>
2024-11-11 11:53:15 +0000

    local docker compose updated and angular environment.development.ts removed

---
+++ deploy-local.sh
@@ -0,0 +1,5 @@
+# Deployment script to run the full stack application using docker compose 
+
+# Compose up the full stack application. Omit certbot. 
+docker compose -f docker-compose.local.yml up frontend nginx backend db -d 
+
