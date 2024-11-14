f7230c4dd2af149184beec95516cdc29768c7807
Andrew <drewstandes@gmail.com>
2024-11-14 19:22:24 +0000

    Slightly altered deployment includes an image prune

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 # Deployment script to run the full stack application using docker compose 
 
+# Stop and remove old containers, keeping volumes intact
+echo "Stopping and removing old containers..."
+docker compose down
+
+# Prune unused images to free up space
+echo "Removing unused Docker images to free up space..."
+docker image prune -af
+
 # Compose up the full stack application. Omit certbot. 
 docker compose up frontend nginx backend db -d 
 
