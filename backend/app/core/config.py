12efbf533f7c1533a61b09bfc2e6aad7f828b3b1
Andrew Smith <91409763+andrewsdsmith@users.noreply.github.com>
2024-12-20 16:07:52 +0000

    aborting integration tests with failed containers

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost"  # For local development
-    ENVIRONMENT: Literal["local", "development", "production"] = "local"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     PROJECT_NAME: str
 
